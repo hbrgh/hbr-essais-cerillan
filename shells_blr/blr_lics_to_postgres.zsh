39f585d7904ed08d135f74b9293520762e67ffba
herve <herve@mac.home>
2026-02-06 08:39:27 +0100

    correction: pas de HEADER

--- shells_blr/blr_lics_to_postgres.zsh
+++ shells_blr/blr_lics_to_postgres.zsh
@@ -21,7 +21,7 @@ psql -p 8611 "dbname=licences user=blr" -f /Users/herve/NOUV_ARARAT_DEV_PERSO/PO
 
 ./list_blr_lic_csv_final.zsh > /tmp/lics.csv
 
-psql -p 8611 "dbname='licences' user='blr'" -c "COPY lics_2026(nom,no_lic,date_naissance,type_lic,categorie,elo) FROM '/tmp/lics.csv'  DELIMITER ';' CSV HEADER;"
+psql -p 8611 "dbname='licences' user='blr'" -c "COPY lics_2026(nom,no_lic,date_naissance,type_lic,categorie,elo) FROM '/tmp/lics.csv'  DELIMITER ';' ;"
 
 
 
