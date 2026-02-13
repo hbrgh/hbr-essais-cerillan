78a6b623458d56081815921616584de0e19748f5
herve <herve@mac-1.home>
2026-02-13 06:29:17 +0100

    remove comment

--- src/main/java/jma/TestPhysicalCores.java
+++ src/main/java/jma/TestPhysicalCores.java
@@ -10,7 +10,6 @@ public class TestPhysicalCores {
 	private static Logger LOGGER = LoggerFactory.getLogger(TestPhysicalCores.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		LOGGER.info("Number of cores = {}", PhysicalCores.count());
 
